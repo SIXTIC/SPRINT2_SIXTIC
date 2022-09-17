ad653604f6b102f75e136371cf19a120c63ad446
Edgar Rodriguez <erodriguez3m@gmail.com>
2022-09-17 14:15:34 -0500

    Se modifican archivos para que corran en jdk 17

---
+++ src/test/java/com/SIXTIC/SIXTIC/SixticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SIXTIC.SIXTIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
